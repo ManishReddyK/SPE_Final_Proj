41fce772c9141afd363d7183f3577c66775793df
ManishReddyK <manish.koppula@iiitb.ac.in>
2023-12-12 16:43:34 +0530

    res

--- backend/src/main/java/com/courier/delivery/utils/JwtFilter.java
+++ backend/src/main/java/com/courier/delivery/utils/JwtFilter.java
@@ -24,7 +24,6 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     private UserDetailsService userService;
 
-    
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         String authorization = httpServletRequest.getHeader("Authorization");
